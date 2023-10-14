119250eb23f9b9d9cfddf7961076d0d0e39e1e0a
James Leahy <defuncart@gmail.com>
2023-10-14 10:08:01 +0200

    Add CustomIcons

---
+++ bin/custom_icons_generate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+icon_font_generator --from=assets_dev/custom_icons --out-font=assets/fonts/custom_icons.ttf --out-flutter=lib/core/theme/custom_icons.dart --class-name=CustomIcons
---
+++ lib/core/theme/custom_icons.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: constant_identifier_names
+@immutable
+class _CustomIconsData extends IconData {
+  const _CustomIconsData(int codePoint, this.name)
+      : super(
+          codePoint,
+          fontFamily: 'CustomIcons',
+        );
+
+  final String name;
+}
+
+@immutable
+class CustomIcons {
+  const CustomIcons._();
+
+  static const answers_layout_horizontal = _CustomIconsData(0xf101, 'answers_layout_horizontal');
+  static const answers_layout_standard = _CustomIconsData(0xf102, 'answers_layout_standard');
+
+  static const all = <String, _CustomIconsData>{
+    'answers_layout_horizontal': answers_layout_horizontal,
+    'answers_layout_standard': answers_layout_standard,
+  };
+}
