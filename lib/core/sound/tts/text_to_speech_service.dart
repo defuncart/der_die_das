2f8bd18dbbab71087c9f83b33e0d8dfa2b5cc7a4
James Leahy <defuncart@gmail.com>
2023-11-18 12:31:39 +0100

    ITextToSpeechService add setSpeechRate

--- lib/core/sound/tts/text_to_speech_service.dart
+++ lib/core/sound/tts/text_to_speech_service.dart
@@ -1,21 +1,39 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+
+enum TextToSpeechRate {
+  oneQuarter(0.25),
+  oneHalf(0.5),
+  threeQuarters(0.75),
+  standard(1.0),
+  oneAndOneQuarter(1.25);
+
+  const TextToSpeechRate(this.value);
+
+  final double value;
+}
 
 abstract class ITextToSpeechService {
   Future<void> init();
   Future<bool> get isSupported;
   Future<void> speak(String text);
+  Future<void> setSpeechRate(TextToSpeechRate rate);
 }
 
 class TextToSpeechService implements ITextToSpeechService {
-  late final FlutterTts _tts;
+  TextToSpeechService({
+    FlutterTts? tts,
+  }) : _tts = tts ?? FlutterTts();
+
+  final FlutterTts _tts;
   late final bool _isSupported;
+  TextToSpeechRate? _rate;
 
   @override
   Future<void> init() async {
-    _tts = FlutterTts();
     // TODO test on non-supported devices
     _isSupported = true;
-    await _tts.setLanguage("de-DE");
+    await _tts.setLanguage('de-DE');
   }
 
   @override
@@ -27,4 +45,23 @@ class TextToSpeechService implements ITextToSpeechService {
       await _tts.speak(text);
     }
   }
+
+  @override
+  Future<void> setSpeechRate(TextToSpeechRate rate) async {
+    if (rate != _rate) {
+      _rate = rate;
+      final normalizedRate = rate.value * normalizedPlatformSpeechRate;
+      await _tts.setSpeechRate(normalizedRate);
+    }
+  }
+
+  /// Speech rate is not uniform across platforms
+  /// https://github.com/dlutton/flutter_tts/issues/109
+  @visibleForTesting
+  double get normalizedPlatformSpeechRate => switch (defaultTargetPlatform) {
+        TargetPlatform.android => 0.8,
+        TargetPlatform.iOS => 0.6,
+        TargetPlatform.macOS => 0.395,
+        _ => 1,
+      };
 }
