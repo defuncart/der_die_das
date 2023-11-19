d43653b349958805a2bc0764620bbb68c0a6b913
James Leahy <defuncart@gmail.com>
2023-11-19 11:25:50 +0100

    Add Hive/Isar dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:der_die_das/core/ui/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final dir = await getApplicationSupportDirectory();
+  Hive.defaultDirectory = dir.path;
+
+  runApp(const ProviderScope(child: MyApp()));
+}
