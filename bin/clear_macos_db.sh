870f627a8ab149cc8936b79895ac935f8578deba
James Leahy <defuncart@gmail.com>
2024-04-20 13:41:44 +0200

    Update bundle id

--- bin/clear_macos_db.sh
+++ bin/clear_macos_db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # on macos deletes the user's database
-rm ~/Library/Containers/com.defuncart.derdiedas.dev/Data/Documents/db.sqlite
+rm ~/Library/Containers/com.defuncart.derdiedasgame/Data/Documents/db.sqlite
--- bin/open_macos_db.sh
+++ bin/open_macos_db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # opens the user's db on macOS in DB Browser
-open -a "DB Browser for SQLite" ~/Library/Containers/com.defuncart.derdiedas.dev/Data/Documents/db.sqlite
+open -a "DB Browser for SQLite" ~/Library/Containers/com.defuncart.derdiedasgame/Data/Documents/db.sqlite
