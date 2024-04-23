02ebeab9cab85494b44913ce02daf4a3c06d5af0
James Leahy <defuncart@gmail.com>
2024-04-23 18:42:55 +0200

    Force portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:der_die_das/core/ui/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_ce/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   final dir = await getApplicationSupportDirectory();
   Hive.init(dir.path);
   await Hive.openBox('settings');
